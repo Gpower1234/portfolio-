f0c46cf6b7884e224a6f6c93116532cd30796d96
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2022-12-10 10:29:18 +0100

    updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,12 @@ DATABASES = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
     }
 }
 
