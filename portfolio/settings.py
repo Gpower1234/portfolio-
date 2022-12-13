1adc1e320f77084702632e3fdabb40bb91ddcb5b
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2022-12-13 07:39:29 +0100

    updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = ['https://Gpee.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://*.Gpee.up.railway.app']
 
 ALLOWED_HOSTS = ['*']
 
