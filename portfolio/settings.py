aad4d11d73fdd6f501156abecb0eb825c62779b0
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2023-04-14 15:17:20 +0100

    updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 from decouple import config
 #from dotenv import load_dotenv
-#import dj_database_url
+import dj_database_url
 
 #DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -26,6 +26,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['*']
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'personal',
     'blog',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,22 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-'''
-DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
-}
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT'),
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
-'''
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
