9fcd472adeba9b041ff61175f513c12c1c0fc485
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2024-03-01 18:58:14 +0100

    updated

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn portfolio.wsgi --log-file -
+release: bash release.sh
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,11 +77,27 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': dj_database_url.parse(config('DATABASE_URL'))
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "loggers": {"": {"handlers": ["console"], "level": "DEBUG"}}
+}
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARED_PROTO", "HTTPS")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
