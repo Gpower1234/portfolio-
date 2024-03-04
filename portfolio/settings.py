0f1ff36376a51a0494dd58e630f306ccd02a1f72
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2024-03-04 15:29:20 +0100

    updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -90,14 +90,6 @@ DATABASES = {
     }
 }
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {"console": {"class": "logging.StreamHandler"}},
-    "loggers": {"": {"handlers": ["console"], "level": "DEBUG"}}
-}
-
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARED_PROTO", "HTTPS")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
