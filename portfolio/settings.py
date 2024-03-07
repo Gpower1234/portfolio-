4aa7c54391abacd8e74d46aecb1aeebdefcc8de7
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2024-03-07 15:36:42 +0100

    updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 #CSRF_TRUSTED_ORIGINS = ['.vercel.app']
 
