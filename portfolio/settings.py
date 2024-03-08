ba39d5a68c956d31fd90a520b5d78e75604cde33
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2024-03-09 00:16:58 +0100

    updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = config('SECRET_KEY')
 #SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
