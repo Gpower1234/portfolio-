54c035075c1c98a30b96e9cb65cda1cc690149cd
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2024-03-09 10:32:08 +0100

    updated

--- personal/models.py
+++ personal/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=30)
-    image = models.ImageField(upload_to='project-image', null=True)
+    image = models.ImageField(upload_to='project-image', blank=True)
     url = models.URLField(blank=True)
 
     def __str__(self):
